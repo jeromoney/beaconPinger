3cc313ce1ffa9b37f90ec4ac10e10e5ddb808836
Justin Matis <justin.matis@gmail.com>
2018-02-09 14:48:22 -0700

    Removed obsolete EWMA function

--- fingerprinter.py
+++ fingerprinter.py
@@ -20,26 +20,6 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-def numpy_ewma_vectorized(data, window):
-
-    alpha = 2 /(window + 1.0)
-    alpha_rev = 1-alpha
-    n = data.shape[0]
-
-    scale = 1/alpha_rev
-    n = data.shape[0]
-
-    r = np.arange(n)
-    scale_arr = scale**r
-    offset = data[0]*alpha_rev**(r+1)
-    pw0 = alpha*alpha_rev**(n-1)
-
-    mult = data*pw0*scale_arr
-    cumsums = mult.cumsum()
-    out = offset + cumsums*scale_arr[::-1]
-    return out
-
-
 class Beacon(object):
     def __init__(self):
         self.id = str(int(time()*1000))
